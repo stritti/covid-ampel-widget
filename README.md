702bfe4cf697aeeb2fd271f297cfb8fa7d0dbb56
Stephan Strittmatter <stephan.strittmatter@sybit.de>
2020-10-23 00:13:39 +0200

    Update about

--- README.md
+++ README.md
@@ -1,24 +1,25 @@
-# covid-ampel-widget
+# Covid Ampel-Widget
+
+This is HTML based widget to show the incidences of German Landkreise for Sars-Cov-2 infections.
+Cases of the last 7 days per 100,000 inhabitants are shown. These is used in Germany to
+define levels for secure each other.
 
 ## Project setup
-```
-npm install
-```
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+This project is based on vue.js V3.
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
     <h3>Über das Corona Ampel-Widget</h3>
+    <p>
+      Das Covid Ampel-Widget basiert auf den Zahlen, die das Robert-Koch-Institut (RKI) für die
+      Landkreise in Deutschland erfasst. Über die Einstellung kann ein Landkreis
+      ausgewählt werden, um die Corona-Ampel für diesen anzuzeigen.
+    </p>
+    <p>
+      Das Covid Ampel Widget ist quelloffen unter der
+      <a href="https://raw.githubusercontent.com/stritti/covid-ampel-widget/main/LICENSE">MIT-Lizenz</a>
+      verfügbar:
+      <a href="https://github.com/stritti/covid-ampel-widget">https://github.com/stritti/covid-ampel-widget</a>
+    </p>
   </div>
 </template>
 
