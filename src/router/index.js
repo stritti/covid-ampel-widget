903cda73c2bf5f66f00ff058637e78d1ec38a314
Stephan Strittmatter <stephan.strittmatter@sybit.de>
2020-10-23 12:19:05 +0200

    update for routing

--- src/router/index.js
+++ src/router/index.js
@@ -2,34 +2,19 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/Home'
 
 const routes = [
-  {
-    path: '/',
-    component: Home
-  },
-  {
-    path: '/lkr/:id',
-    component: Home
-  },
-  {
-    path: '/config',
-    component: () => import(/* webpackChunkName: "config" */ '../views/Config.vue')
-  },
-  {
-    path: '/about',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/imprint',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Imprint.vue')
-  },
-  {
-    path: '/privacy',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Privacy.vue')
-  }
+  { path: '/', component: Home },
+  { path: '/lkr/:id', component: Home },
+  { path: '/config', component: () => import(/* webpackChunkName: "config" */ '../views/Config.vue') },
+  { path: '/about', component: () => import(/* webpackChunkName: "about" */ '../views/About.vue') },
+  { path: '/imprint', component: () => import(/* webpackChunkName: "about" */ '../views/Imprint.vue') },
+  { path: '/privacy', component: () => import(/* webpackChunkName: "about" */ '../views/Privacy.vue') },
+  { path: '/:pathMatch(.*)', component: Home }
 ]
 
+// const publicPath = process.env.NODE_ENV === 'production' ? '/covid-ampel-widget/' : '/'
+
 const router = createRouter({
-  history: createWebHistory('/covid-ampel-widget/'),
+  history: createWebHistory(),
   routes: routes,
 })
 
