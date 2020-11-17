0e302ce86ecf3e7e67f3a5715eef9ec7b8f8fccb
Stephan Strittmatter <stritti@users.noreply.github.com>
2020-11-17 10:41:13 +0100

    initial add of google analytics

--- src/main.js
+++ src/main.js
@@ -2,10 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import vantPlugin from './plugins/vant'
+import VueAnalytics from 'vue-analytics'
 
 const app = createApp(App)
 
 app.use(router)
 app.use(vantPlugin)
+app.use(VueAnalytics, {
+  id: 'G-GDDZX0T7FP',
+  router,
+  autoTracking: {
+    screenview: true
+  }
+})
 
 app.mount('#app')
