e206b3514d8c6ea597295ba6d9b53da8dcab11ab
Stephan Strittmatter <stritti@users.noreply.github.com>
2021-01-17 01:40:51 +0100

    Add ko-fi

--- src/views/About.vue
+++ src/views/About.vue
@@ -49,6 +49,18 @@
         <a href="https://github.com/stritti/covid-ampel-widget">GitHub</a>
         verfügbar.
       </p>
+      <p>
+        <a
+          href="https://ko-fi.com/J3J33A8DT"
+          target="_blank"
+        ><img
+          height="36"
+          style="border:0px;height:36px;"
+          src="https://cdn.ko-fi.com/cdn/kofi1.png?v=2"
+          border="0"
+          alt="Buy Me a Coffee at ko-fi.com"
+        ></a>
+      </p>
     </div>
   </div>
 </template>
