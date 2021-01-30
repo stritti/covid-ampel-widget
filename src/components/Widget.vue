eb145f6d74b5d1bef9de5206cbe7290596fd51e7
Stephan Strittmatter <stritti@users.noreply.github.com>
2021-01-30 20:48:32 +0100

    links tyle repaired

--- src/components/Widget.vue
+++ src/components/Widget.vue
@@ -55,6 +55,7 @@
               <span class="label">Datenquelle: </span>
               <span class="data">
                 <a
+                  :class="incidenceColor"
                   target="_blank"
                   rel="noreferrer"
                   href="https://experience.arcgis.com/experience/478220a4c454480e823b17327b2bf1d4/page/page_1/"
