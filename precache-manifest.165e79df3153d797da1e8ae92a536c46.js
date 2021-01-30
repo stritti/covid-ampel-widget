0727df9914208f7dbe909c1da4173bbeb275a847
stritti <stritti@users.noreply.github.com>
2021-01-30 12:16:52 +0000

    deploy: c39a77113b824677db8da569b80d83455baf94b3

---
+++ precache-manifest.165e79df3153d797da1e8ae92a536c46.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d986cd4a28630e8f6eb",
+    "url": "/covid-ampel-widget/css/about~33d48c78.6b398eaf.css"
+  },
+  {
+    "revision": "c049efe45ad829f68334",
+    "url": "/covid-ampel-widget/css/app~d0ae3f07.ac59fdd7.css"
+  },
+  {
+    "revision": "8625adc849a6195906fb",
+    "url": "/covid-ampel-widget/css/config~21833f8f.f858318d.css"
+  },
+  {
+    "revision": "a66e24ea7819d5e551ef",
+    "url": "/covid-ampel-widget/css/help~f71cff67.33b79095.css"
+  },
+  {
+    "revision": "e0def45df3ddb83cb9d9",
+    "url": "/covid-ampel-widget/css/npm.vant~253ae210.73e07480.css"
+  },
+  {
+    "revision": "3cfb01be9c9a5d17bca1",
+    "url": "/covid-ampel-widget/css/npm.vant~67ff1999.fb3973f2.css"
+  },
+  {
+    "revision": "a39924373c67176712f25013acca2318",
+    "url": "/covid-ampel-widget/help/IMG_6911.jpg"
+  },
+  {
+    "revision": "88a6ddbd0cb241a7eac309d4977b0e70",
+    "url": "/covid-ampel-widget/help/IMG_6912.jpg"
+  },
+  {
+    "revision": "a6a4399b81443c303e5249099a559402",
+    "url": "/covid-ampel-widget/help/IMG_6915.jpg"
+  },
+  {
+    "revision": "f5aebe4691612d8c83b37d59d937e09b",
+    "url": "/covid-ampel-widget/help/IMG_6916.jpg"
+  },
+  {
+    "revision": "0e8b41e9c50e0b92a4697eba5ffc69a3",
+    "url": "/covid-ampel-widget/help/IMG_6917.jpg"
+  },
+  {
+    "revision": "bc5487b7f541ca207bc641f2fdb72c73",
+    "url": "/covid-ampel-widget/help/android-configure-1.png"
+  },
+  {
+    "revision": "bc5487b7f541ca207bc641f2fdb72c73",
+    "url": "/covid-ampel-widget/help/android-configure-2.png"
+  },
+  {
+    "revision": "bc5487b7f541ca207bc641f2fdb72c73",
+    "url": "/covid-ampel-widget/help/android-configure-3.png"
+  },
+  {
+    "revision": "8eedd252a40ba3103dda76a2dc25df8f",
+    "url": "/covid-ampel-widget/help/android-covid-19-ampel-widget.png"
+  },
+  {
+    "revision": "8031a370ac55e91b548a763c05cd6791",
+    "url": "/covid-ampel-widget/help/android-home.png"
+  },
+  {
+    "revision": "1d15116551dd65ed4c9d486fe34da7c5",
+    "url": "/covid-ampel-widget/help/android-web-widget.png"
+  },
+  {
+    "revision": "b987d220c3f50f588f81798f64404a67",
+    "url": "/covid-ampel-widget/help/ios-glimpse-settings.jpeg"
+  },
+  {
+    "revision": "3fd2db0cf2ad23d3d61e5a10f9182b40",
+    "url": "/covid-ampel-widget/img/coronaampel.3fd2db0c.png"
+  },
+  {
+    "revision": "f5aebe4691612d8c83b37d59d937e09b",
+    "url": "/covid-ampel-widget/img/ios-add-to-glimpse.f5aebe46.jpg"
+  },
+  {
+    "revision": "88a6ddbd0cb241a7eac309d4977b0e70",
+    "url": "/covid-ampel-widget/img/ios-app-glimpse.88a6ddbd.jpg"
+  },
+  {
+    "revision": "a39924373c67176712f25013acca2318",
+    "url": "/covid-ampel-widget/img/ios-covid-19-ampel-widget.a3992437.jpg"
+  },
+  {
+    "revision": "0e8b41e9c50e0b92a4697eba5ffc69a3",
+    "url": "/covid-ampel-widget/img/ios-glimpse-add-widget.0e8b41e9.jpg"
+  },
+  {
+    "revision": "b987d220c3f50f588f81798f64404a67",
+    "url": "/covid-ampel-widget/img/ios-glimpse-settings.b987d220.jpg"
+  },
+  {
+    "revision": "a6a4399b81443c303e5249099a559402",
+    "url": "/covid-ampel-widget/img/ios-share.a6a4399b.jpg"
+  },
+  {
+    "revision": "3af2560492d49db6bf3a4ac8ef8753c1",
+    "url": "/covid-ampel-widget/index.css"
+  },
+  {
+    "revision": "4dfe748c95c3ad11f03db9a3480a6e9a",
+    "url": "/covid-ampel-widget/index.html"
+  },
+  {
+    "revision": "6d986cd4a28630e8f6eb",
+    "url": "/covid-ampel-widget/js/about~33d48c78.062d99a6.js"
+  },
+  {
+    "revision": "c049efe45ad829f68334",
+    "url": "/covid-ampel-widget/js/app~d0ae3f07.7075f6bf.js"
+  },
+  {
+    "revision": "8625adc849a6195906fb",
+    "url": "/covid-ampel-widget/js/config~21833f8f.54890323.js"
+  },
+  {
+    "revision": "a66e24ea7819d5e551ef",
+    "url": "/covid-ampel-widget/js/help~f71cff67.e40fac58.js"
+  },
+  {
+    "revision": "ced5bc723e64c5fe254a",
+    "url": "/covid-ampel-widget/js/npm.axios~e4173fa2.95f2a477.js"
+  },
+  {
+    "revision": "53b85a19beac5ae7c8f2",
+    "url": "/covid-ampel-widget/js/npm.babel~f269b12e.da9a45d0.js"
+  },
+  {
+    "revision": "3fe187b5e6cc11cc9aef",
+    "url": "/covid-ampel-widget/js/npm.core-js~987e6011.2d1c4d59.js"
+  },
+  {
+    "revision": "664fdbed51117cba6b05",
+    "url": "/covid-ampel-widget/js/npm.dexie~4f51174a.61d4188d.js"
+  },
+  {
+    "revision": "db761fad8d22d096dd8d",
+    "url": "/covid-ampel-widget/js/npm.node-libs-browser~7312b895.889b5868.js"
+  },
+  {
+    "revision": "7122342d100e2552903d",
+    "url": "/covid-ampel-widget/js/npm.path-browserify~4fd1e1ee.f4a6f7c7.js"
+  },
+  {
+    "revision": "27e348f8820176b2ba60",
+    "url": "/covid-ampel-widget/js/npm.regenerator-runtime~ccfd30e1.abd5e6b2.js"
+  },
+  {
+    "revision": "8228003943394eb4e0ac",
+    "url": "/covid-ampel-widget/js/npm.register-service-worker~7fc74268.fded5507.js"
+  },
+  {
+    "revision": "e0def45df3ddb83cb9d9",
+    "url": "/covid-ampel-widget/js/npm.vant~253ae210.9216cfb2.js"
+  },
+  {
+    "revision": "3cfb01be9c9a5d17bca1",
+    "url": "/covid-ampel-widget/js/npm.vant~67ff1999.c3c4aff4.js"
+  },
+  {
+    "revision": "9249332accef789d3b8c",
+    "url": "/covid-ampel-widget/js/npm.vue-crono~d63dbf10.74635a58.js"
+  },
+  {
+    "revision": "d09e96a8754182e0a72b",
+    "url": "/covid-ampel-widget/js/npm.vue-gtag-next~2584efac.acff979a.js"
+  },
+  {
+    "revision": "88ef32f06b16df0fa402",
+    "url": "/covid-ampel-widget/js/npm.vue-router~aacd4aef.e3b30a2f.js"
+  },
+  {
+    "revision": "00f4632f4c9e9ede09f8",
+    "url": "/covid-ampel-widget/js/npm.vue~253ae210.5ae5acf1.js"
+  },
+  {
+    "revision": "0da17f5f4eba93b86315",
+    "url": "/covid-ampel-widget/js/npm.vue~5ea1a303.fb144ba9.js"
+  },
+  {
+    "revision": "96d3a4861a458ef090a7",
+    "url": "/covid-ampel-widget/js/npm.webpack~0faa04c6.d445adad.js"
+  },
+  {
+    "revision": "1075627d8323d3188605",
+    "url": "/covid-ampel-widget/js/runtime.1f43ea02.js"
+  },
+  {
+    "revision": "8e181fa90eb053ba50d4e3423c892fa2",
+    "url": "/covid-ampel-widget/manifest.json"
+  },
+  {
+    "revision": "a8b1a53a286c334097a7f22020b3baf7",
+    "url": "/covid-ampel-widget/reset.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/covid-ampel-widget/robots.txt"
+  },
+  {
+    "revision": "3fd2db0cf2ad23d3d61e5a10f9182b40",
+    "url": "/covid-ampel-widget/thumb.png"
+  }
+]);
