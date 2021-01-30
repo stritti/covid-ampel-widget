997afc9398a01b65da53c3650a3372070acab26e
Stephan Strittmatter <stritti@users.noreply.github.com>
2021-01-30 20:43:37 +0100

    Update Widget.vue

--- src/components/Widget.vue
+++ src/components/Widget.vue
@@ -55,7 +55,6 @@
               <span class="label">Datenquelle: </span>
               <span class="data">
                 <a
-                  :class="widgetClass(data.cases7_per_100k)"
                   target="_blank"
                   rel="noreferrer"
                   href="https://experience.arcgis.com/experience/478220a4c454480e823b17327b2bf1d4/page/page_1/"
