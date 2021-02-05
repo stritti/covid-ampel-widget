2c41fddc9dbb8885dae47dc31872d120ea32662b
Stephan Strittmatter <stritti@users.noreply.github.com>
2021-02-05 15:53:59 +0100

    add afe-area-inset-top for top nav bar

--- src/App.vue
+++ src/App.vue
@@ -20,3 +20,18 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+  .van-nav-bar {
+// Browsers which partially support CSS Environment variables (iOS 11.0-11.2).
+  @supports (padding-top: env(safe-area-inset-top)) {
+    --safe-area-inset-top: env(safe-area-inset-bottom);
+    padding-top: calc(var(--safe-area-inset-top));
+  }
+  // Browsers which fully support CSS Environment variables (iOS 11.2+).
+  @supports (padding-top: constant(safe-area-inset-top)) {
+    --safe-area-inset-top: constant(safe-area-inset-top);
+    padding-top: calc(var(--safe-area-inset-top) );
+  }
+  }
+</style>
