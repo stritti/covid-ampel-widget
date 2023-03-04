203f5c1621fe4b9b92e114a5a3833671a4697b05
Stephan Strittmatter <stritti@users.noreply.github.com>
2023-03-04 23:18:12 +0000

    fix

--- vue.config.js
+++ vue.config.js
@@ -19,27 +19,7 @@ module.exports = {
   },
 
   configureWebpack: {
-    plugins: [new webpack.IgnorePlugin({ resourceRegExp: /\.\/locale$/, contextRegExp: /moment$/ })],
-    optimization: {
-      runtimeChunk: 'single',
-      splitChunks: {
-        chunks: 'all',
-        maxInitialRequests: Infinity,
-        minSize: 0,
-        maxSize: 200000,
-        cacheGroups: {
-          vendor: {
-            test: /[\\/]node_modules[\\/]/,
-            name (module) {
-              const packageName = module.context.match(
-                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-              )[1]
-              return `npm.${packageName.replace('@', '')}`
-            }
-          }
-        }
-      }
-    }
+    plugins: [new webpack.IgnorePlugin({ resourceRegExp: /\.\/locale$/, contextRegExp: /moment$/ })]
   },
 
   publicPath: process.env.VUE_APP_BASE_PATH
